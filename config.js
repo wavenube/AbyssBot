c6f8434351b5fdc1226b9a289ac6fd3923a261a4
wavenube <wavenub3@gmail.com>
2024-07-31 16:36:08 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url' 
 
 global.owner = [
-  ['59172945992', 'FG98', true],
+  ['34682075812', 'FG98', true],
   ['5491168352204']
 ] //Numeros de owner 
 
