370ad173a877dcb02b741af08f1f6cc4dac30d99
wavenube <wavenub3@gmail.com>
2024-08-01 15:54:19 +0100

    Create main-subbot.js

---
+++ plugins/main-subbot.js
@@ -0,0 +1,42 @@
+let handler = async (m, { conn, usedPrefix, text, args, command }) => {
+    // Mensaje principal
+    let mainMessage = 'Selecciona el menú que deseas obtener:';
+
+    // Opciones del menú desplegable
+    let listSections = [
+        {
+            title: "SER SUBBOT",
+            rows: [
+                {
+                    title: "BOTCLONE",
+                    description: "Clona el bot.",
+                    id: `${usedPrefix}botclone`
+                },
+                {
+                    title: "BOTLIST",
+                    description: "Muestra la lista de bots.",
+                    id: `${usedPrefix}botlist`
+                },
+                {
+                    title: "TXBOT",
+                    description: "Transfiere el bot a otro número.",
+                    id: `${usedPrefix}txbot`
+                },
+                {
+                    title: "STOP",
+                    description: "Detiene el bot.",
+                    id: `${usedPrefix}stop`
+                }
+            ]
+        }
+    ];
+
+    // Enviar el menú desplegable
+    await conn.sendList(m.chat, '  ≡ *Menú Desplegable*', `\n ${mainMessage}`, 'Selecciona una opción', null, listSections, m);
+};
+
+handler.help = ['menubot']
+handler.tags = ['admin']
+handler.command = ['menubot', 'botmenu']
+
+export default handler;
