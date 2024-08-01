eb1e6fafb11210310b808feeb73f3089fc5574fe
wavenube <wavenub3@gmail.com>
2024-08-01 19:46:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -24,12 +24,12 @@ const rl = createInterface(process.stdin, process.stdout)
 const app = express()
 const port = process.env.PORT || 8080;
 
-say('Senna FG98', {
+say('Abyss Bot', {
   font: 'pallet',
   align: 'center',
   gradient: ['red', 'magenta']
 })
-say(`senna-bot By FG98 Ig: @fg98_ff`, {
+say(`beta`, {
   font: 'console',
   align: 'center',
   gradient: ['cyan', 'magenta']
