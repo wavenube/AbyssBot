af503e902d6d0a402be8f3e83127cff155adc84b
wavenube <wavenub3@gmail.com>
2024-08-01 13:35:26 +0100

    Update tools-raid.js

--- plugins/tools-raid.js
+++ plugins/tools-raid.js
@@ -5,7 +5,7 @@ const handler = async (m, { conn, participants, usedPrefix, command }) => {
     const chatId = m.chat;
 
     // Verificar si el bot tiene permisos restrictivos habilitados
-    if (!global.db.data.settings[conn.user.jid].restrict) throw `${lenguajeGB['smsAvisoAG']()}${lenguajeGB['smsSoloOwner']()}`;
+    
 
     // Verificar si el comando está en cooldown para el grupo
     if (cooldown[chatId] && (Date.now() - cooldown[chatId]) < COOLDOWN_TIME) {
