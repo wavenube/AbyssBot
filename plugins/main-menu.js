dc578c9f0ea371268e0e804acc9d7623bb0f07ae
wavenube <wavenub3@gmail.com>
2024-08-01 16:17:01 +0100

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -1,6 +1,8 @@
 let handler = async (m, { conn, usedPrefix, text, args, command }) => {
     // Mensaje principal
     let mainMessage = 'Selecciona el menú que deseas obtener:';
+        let pp = './src/abyss.png';
+
 
     // Opciones del menú desplegable
     let listSections = [
