7570d1dbff71213990bf08ab7fc1732916a731c7
wavenube <wavenub3@gmail.com>
2024-08-01 12:00:44 +0100

    Update main-menu2.js

--- plugins/main-menu2.js
+++ plugins/main-menu2.js
@@ -11,6 +11,7 @@ let handler = async function (m, { conn, text, usedPrefix }) {
   try {
     let user = global.db.data.users[who]
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+    if (!(who in global.db.data.users)) throw `✳️ ${mssg.userDb}`
     let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {};
     let { exp, diamond, level, role } = global.db.data.users[m.sender];
     let { min, xp, max } = xpRange(level, global.multiplier);
