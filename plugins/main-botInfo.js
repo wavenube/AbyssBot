cd76c7fbedc22327bcc072f3ca34df2185c78f28
wavenube <wavenub3@gmail.com>
2024-08-02 19:32:47 +0100

    Update main-botInfo.js

--- plugins/main-botInfo.js
+++ plugins/main-botInfo.js
@@ -53,16 +53,10 @@ let infobt = `
   *FG98*
 ▢ Instagram :
   • ${fgig}
-▢ Telegram : 
-  • t.me/fgsupp_bot (FG) 
-  • t.me/fg98ff (canal)
-  • t.me/fgawgp (grupo)
-▢ YouTube : 
-  • https://youtube.com/fg98f
   
  *≡ HELPERS*
-  *ANDREA*
-▢ WhatsApp : wa.me/573125484672`
+  *XPATOH*
+▢ WhatsApp : wa.me/34674339494`
 
 /*conn.sendButton(m.chat, infobt, mssg.ig, null, [
   ['ꨄ︎ Apoyar', `${usedPrefix}donate`],
