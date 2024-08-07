bcca076998c04f4473c7be4a25b60b05eeb475ef
wavenube <wavenub3@gmail.com>
2024-08-07 16:31:28 +0100

    Update av-audios.js

--- plugins/av-audios.js
+++ plugins/av-audios.js
@@ -14,10 +14,10 @@ export default handler
 
 
 let audioMsg = {
-  'fino señores': './src/mp3/fino.mp3',
+  'fino': './src/mp3/fino.mp3',
   'buenos días': 'https://k.top4top.io/m_2826iqdri1.mp3',
   'buenas tardes': 'https://b.top4top.io/m_2826v2zg51.mp3',
   'buenas noches': 'https://i.top4top.io/m_2826o8rfj1.mp3',
   'sad': 'https://h.top4top.io/m_2826mcim21.mp3',
-  '@5491168352204|@59172945992': 'https://l.top4top.io/m_2492i4mdu1.mp3'
+  'pela': './src/mp3/melapela.mp3'
 }
