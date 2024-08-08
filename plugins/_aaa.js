328ba2a76b6d5fdeb79f79eef3f62800fe2352d8
wavenube <wavenub3@gmail.com>
2024-08-08 15:21:42 +0100

    Update _aaa.js

--- plugins/_aaa.js
+++ plugins/_aaa.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const mediaPath = path.join(__dirname, '../src/mp3');
+const mediaPath = path.join(__dirname, './src/mp3');
 
 const handler = (m) => m;
 
