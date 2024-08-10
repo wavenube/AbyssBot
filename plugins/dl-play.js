6cd33d34722e9c7a17bd127f32e2264b3f7fefa2
wavenube <wavenub3@gmail.com>
2024-08-10 13:48:21 +0100

    Update dl-play.js

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -1,56 +1,49 @@
-import fetch from 'node-fetch';
 import yts from 'yt-search';
-import ytdl from 'ytdl-core';
-import axios from 'axios';
 import { youtubedl, youtubedlv2 } from '@bochilteam/scraper';
 
 const handler = async (m, { conn, command, args, text, usedPrefix }) => {
+  if (!text) return conn.reply(m.chat, `*🤔 ¿Qué estás buscando?*\n*Ingrese el nombre de la canción*\n\n*Ejemplo:*\n#play emilia 420`, m);
 
-  if (command === 'play' || command === 'play2') {
-    if (!text) {
-      return conn.reply(m.chat, `*🤔 ¿Qué estás buscando? 🤔*\n*Ingresa el nombre de la canción*\n\n*Ejemplo:*\n#play emilia 420`, m);
-    }
-    const yt_play = await search(args.join(' '));
-    const texto1 = `📌 *Título* : ${yt_play[0].title}\n📆 *Publicado:* ${yt_play[0].ago}\n⌚ *Duración:* ${secondString(yt_play[0].duration.seconds)}`.trim();
+  const yt_play = await search(args.join(' '));
+  if (yt_play.length === 0) return conn.reply(m.chat, '⚠️ No se encontraron resultados.', m);
 
-    await conn.sendButton(m.chat, texto1, null, yt_play[0].thumbnail, [
-      ['Audio', `${usedPrefix}ytmp3 ${yt_play[0].url}`],
-      ['Video', `${usedPrefix}ytmp4 ${yt_play[0].url}`]
-    ], null, m);
-  }
+  const video = yt_play[0];
+  const texto1 = `📌 *Título*: ${video.title}\n📆 *Publicado*: ${video.ago}\n⌚ *Duración*: ${secondString(video.duration.seconds)}`;
+
+  const buttons = [
+    { buttonId: `${usedPrefix}ytmp3 ${video.url}`, buttonText: { displayText: 'Audio' }, type: 1 },
+    { buttonId: `${usedPrefix}ytmp4 ${video.url}`, buttonText: { displayText: 'Video' }, type: 1 },
+  ];
 
   if (command === 'play3' || command === 'play4') {
-    if (!text) {
-      return conn.reply(m.chat, `*🤔 ¿Qué estás buscando? 🤔*\n*Ingresa el nombre de la canción*\n\n*Ejemplo:*\n#play emilia 420`, m);
-    }
-    const yt_play = await search(args.join(' '));
-    const texto1 = `📌 *Título* : ${yt_play[0].title}\n📆 *Publicado:* ${yt_play[0].ago}\n⌚ *Duración:* ${secondString(yt_play[0].duration.seconds)}\n👀 *Vistas:* ${MilesNumber(yt_play[0].views)}`.trim();
-
-    await conn.sendButton(m.chat, texto1, null, yt_play[0].thumbnail, [
-      ['Audio', `${usedPrefix}ytmp3 ${yt_play[0].url}`],
-      ['Video', `${usedPrefix}ytmp4 ${yt_play[0].url}`],
-      ['Más resultados', `${usedPrefix}yts ${text}`]
-    ], null, m);
+    texto1 += `\n👀 *Vistas*: ${MilesNumber(video.views)}`;
+    buttons.push({ buttonId: `${usedPrefix}yts ${text}`, buttonText: { displayText: 'Más resultados' }, type: 1 });
   }
+
+  const buttonMessage = {
+    image: { url: video.thumbnail },
+    caption: texto1,
+    footer: '',
+    buttons: buttons,
+    headerType: 4,
+  };
+
+  await conn.sendMessage(m.chat, buttonMessage, { quoted: m });
 };
 
-handler.help = ['play', 'play2'];
+handler.help = ['play', 'play2', 'play3', 'play4'];
 handler.tags = ['downloader'];
 handler.command = ['play', 'play2', 'play3', 'play4'];
-handler.register = true;
 export default handler;
 
 async function search(query, options = {}) {
-  const search = await yts.search({ query, hl: 'es', gl: 'ES', ...options });
-  return search.videos;
+  const searchResults = await yts.search({ query, hl: 'es', gl: 'ES', ...options });
+  return searchResults.videos;
 }
 
 function MilesNumber(number) {
   const exp = /(\d)(?=(\d{3})+(?!\d))/g;
-  const rep = '$1.';
-  const arr = number.toString().split('.');
-  arr[0] = arr[0].replace(exp, rep);
-  return arr[1] ? arr.join('.') : arr[0];
+  return number.toString().replace(exp, '$1.');
 }
 
 function secondString(seconds) {
@@ -59,9 +52,5 @@ function secondString(seconds) {
   const h = Math.floor((seconds % (3600 * 24)) / 3600);
   const m = Math.floor((seconds % 3600) / 60);
   const s = Math.floor(seconds % 60);
-  const dDisplay = d > 0 ? d + (d === 1 ? ' día, ' : ' días, ') : '';
-  const hDisplay = h > 0 ? h + (h === 1 ? ' hora, ' : ' horas, ') : '';
-  const mDisplay = m > 0 ? m + (m === 1 ? ' minuto, ' : ' minutos, ') : '';
-  const sDisplay = s > 0 ? s + (s === 1 ? ' segundo' : ' segundos') : '';
-  return dDisplay + hDisplay + mDisplay + sDisplay;
+  return `${d > 0 ? `${d} día${d > 1 ? 's' : ''}, ` : ''}${h > 0 ? `${h} hora${h > 1 ? 's' : ''}, ` : ''}${m > 0 ? `${m} minuto${m > 1 ? 's' : ''}, ` : ''}${s > 0 ? `${s} segundo${s > 1 ? 's' : ''}` : ''}`.trim();
 }
