1e45174e2d97febddab10b2f4261578a2029f712
wavenube <wavenub3@gmail.com>
2024-08-10 13:43:06 +0100

    Update dl-play.js

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -1,30 +1,50 @@
+import fetch from 'node-fetch';
+import yts from 'yt-search';
+import ytdl from 'ytdl-core';
+import axios from 'axios';
+import { youtubedl, youtubedlv2 } from '@bochilteam/scraper';
 
-import yts from 'yt-search'
-let handler = async (m, { conn, command, text, usedPrefix }) => {
-	
-  if (!text) throw `✳️ ${mssg.example} *${usedPrefix + command}* Lil Peep hate my life`
-	let res = await yts(text)
-	let vid = res.videos[0]
-	if (!vid) throw `✳️ Vídeo/Audio no encontrado`
-	let { title, description, thumbnail, videoId, timestamp, views, ago, url } = vid
-	//const url = 'https://www.youtube.com/watch?v=' + videoId
-	m.react('🎧') 
-  let play = `
-	≡ *FG MUSIC*
-┌──────────────
-▢ 📌 *${mssg.title}:* ${vid.title}
-▢ 📆 *${mssg.aploud}:* ${vid.ago}
-▢ ⌚ *${mssg.duration}:* ${vid.timestamp}
-▢ 👀 *${mssg.views}:* ${vid.views.toLocaleString()}
-└──────────────`
- await conn.sendButton2(m.chat, play, mssg.ig, thumbnail, [
-    ['🎶 MP3', `${usedPrefix}fgmp3 ${url}`],
-    ['🎥 MP4', `${usedPrefix}fgmp4 ${url}`]
-  ], null, [['Canal', `${fgcanal}`]], m)
+const handler = async (m, { conn, command, args, text, usedPrefix }) => {
+  if (!text) return conn.reply(m.chat, `*🤔¿Qué estás buscando?*\n*Ingrese el nombre de la canción*\n\n*Ejemplo:*\n#play emilia 420`, m);
+
+  const yt_play = await search(args.join(' '));
+  if (yt_play.length === 0) return conn.reply(m.chat, '⚠️ No se encontraron resultados.', m);
+
+  const texto1 = `📌 *Título*: ${yt_play[0].title}\n📆 *Publicado*: ${yt_play[0].ago}\n⌚ *Duración*: ${secondString(yt_play[0].duration.seconds)}`.trim();
+
+  const buttons = [
+    ['Audio', `${usedPrefix}ytmp3 ${yt_play[0].url}`],
+    ['Video', `${usedPrefix}ytmp4 ${yt_play[0].url}`],
+  ];
+
+  if (command === 'play3' || command === 'play4') {
+    texto1 += `\n👀 *Vistas*: ${MilesNumber(yt_play[0].views)}`;
+    buttons.push(['Más resultados', `${usedPrefix}yts ${text}`]);
+  }
+
+  await conn.sendButton(m.chat, texto1, '', yt_play[0].thumbnail, buttons, m);
+};
+
+handler.help = ['playvid', 'play2', 'play3', 'play4'];
+handler.tags = ['downloader'];
+handler.command = ['play', 'play2', 'play3', 'play4'];
+export default handler;
+
+async function search(query, options = {}) {
+  const searchResults = await yts.search({ query, hl: 'es', gl: 'ES', ...options });
+  return searchResults.videos;
 }
-handler.help = ['play']
-handler.tags = ['dl']
-handler.command = ['play', 'playvid']
-handler.disabled = false
 
-export default handler
+function MilesNumber(number) {
+  const exp = /(\d)(?=(\d{3})+(?!\d))/g;
+  return number.toString().replace(exp, '$1.');
+}
+
+function secondString(seconds) {
+  seconds = Number(seconds);
+  const d = Math.floor(seconds / (3600 * 24));
+  const h = Math.floor((seconds % (3600 * 24)) / 3600);
+  const m = Math.floor((seconds % 3600) / 60);
+  const s = Math.floor(seconds % 60);
+  return `${d > 0 ? `${d} día${d > 1 ? 's' : ''}, ` : ''}${h > 0 ? `${h} hora${h > 1 ? 's' : ''}, ` : ''}${m > 0 ? `${m} minuto${m > 1 ? 's' : ''}, ` : ''}${s > 0 ? `${s} segundo${s > 1 ? 's' : ''}` : ''}`.trim();
+}
